2dd6b8ac35f7f6adbfe23e6898449e714780486e
seonggon do <mooh2jj@naver.com>
2024-01-16 00:13:24 +0900

    update: initData 추가

--- src/main/java/com/example/dockertest/dto/request/BlogCreateRequest.java
+++ src/main/java/com/example/dockertest/dto/request/BlogCreateRequest.java
@@ -1,16 +1,20 @@
 package com.example.dockertest.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 @Setter
 @Getter
+@AllArgsConstructor
 @ToString
 public class BlogCreateRequest {
 
     private String title;
 
     private String content;
+
+
 
 }
